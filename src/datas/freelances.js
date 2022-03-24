001d333dc7cce42cda6ae095ec6bcef04de8ecd3
Virtus-web <nicolas.pedenau@hotmail.fr>
2022-03-24 10:56:45 +0100

    change pictures avatar

--- src/datas/freelances.js
+++ src/datas/freelances.js
@@ -3,66 +3,66 @@ export const freelancersList = [
         id: "1",
         name: "Julien Brun",
         job: "Développeur mobile",
-        picture: "http://localhost:8000/images/4.jpeg"
+        picture: "../../assets/avatar1.jpg"
     },
     {
         id: "2",
         name: "Arielle Gautier",
         job: "Développeuse fullstack",
-        picture: "http://localhost:8000/images/1.jpeg"
+        picture: "../../assets/avatar2.jpg"
     },
     {
         id: "3",
         name: "Marine Carpentier",
         job: "Développeuse frontend",
-        picture: "http://localhost:8000/images/2.jpeg"
+        picture: "../../assets/avatar3.jpg"
     },
     {
         id: "4",
         name: "Lucille Barre",
         job: "Product Designer",
-        picture: "http://localhost:8000/images/3.jpeg"
+        picture: "../../assets/avatar4.jpg"
     },
     {
         id: "5",
         name: "Clément Rolland",
         job: "Développeur mobile",
-        picture: "http://localhost:8000/images/5.jpeg"
+        picture: "../../assets/avatar5.jpg"
     },
     {
         id: "6",
         name: "Grégoire Chevalier",
         job: "Développeur backend",
-        picture: "http://localhost:8000/images/6.jpeg"
+        picture: "../../assets/avatar6.jpg"
     },
     {
         id: "7",
         name: "Raphaël Rodriguez",
         job: "Designer",
-        picture: "http://localhost:8000/images/7.jpeg"
+        picture: "../../assets/avatar7.jpg"
     },
     {
         id: "8",
         name: "Hugo Vysa",
         job: "Développeur frontend",
-        picture: "http://localhost:8000/images/8.jpeg"
+        picture: "../../assets/avatar8.jpg"
     },
     {
         id: "9",
         name: "Mina Toman",
         job: "Développeuse Mobile",
-        picture: "http://localhost:8000/images/11.jpeg"
+        picture: "../../assets/avatar9.jpg"
     },
     {
         id: "10",
         name: "Amélie Leroy",
         job: "Développeuse backend",
-        picture: "http://localhost:8000/images/12.jpeg"
+        picture: "../../assets/avatar10.jpg"
     },
     {
         id: "11",
         name: "Maxime Lebrun",
         job: "Intégrateur SEO",
-        picture: "http://localhost:8000/images/9.jpeg"
+        picture: "../../assets/avatar11.jpg"
     }
 ]
